797bde5bd9c3323d83426ae9ce0dbd26e1ff5bae
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-08-11 14:54:13 -0400

    added: sample Tailwind nav bar

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,8 +2,66 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <nav class="flex items-center justify-between p-6 bg-black">
+      <div>
+        <h1 class="text-white">ACM</h1>
+      </div>
+      <div>
+        <ul class="flex align-middle">
+          <li class="ml-12">
+            <a
+              class="text-white hover:no-underline visited:text-white"
+              href="/"
+            >
+              HOME
+            </a>
+          </li>
+          <li class="ml-12">
+            <a
+              class="text-white hover:no-underline visited:text-white"
+              href="/"
+            >
+              CALENDAR
+            </a>
+          </li>
+          <li class="ml-12">
+            <a
+              class="text-gray-400 cursor-not-allowed visited:text-white"
+              href="/"
+            >
+              FEATURED EVENTS
+            </a>
+          </li>
+          <li class="ml-12">
+            <a
+              class="text-gray-400 cursor-not-allowed visited:text-white"
+              href="/"
+            >
+              RESOURCES
+            </a>
+          </li>
+          <li class="ml-12">
+            <a
+              class="text-gray-400 cursor-not-allowed visited:text-white"
+              href="/"
+            >
+              ABOUT US
+            </a>
+          </li>
+        </ul>
+      </div>
+      <style jsx>
+        {`
+          nav {
+            position: fixed;
+            width: 100%;
+            height: 4em;
+          }
+          a:visited {
+            color: white;
+          }
+        `}
+      </style>
+    </nav>
   )
 }
